00317e6ccd784e655d1fb166faba443e9f7b102c
AreenAl <areenaldda@gmail.com>
2024-02-20 13:25:14 +0200

    divide rout

---
+++ encryption.py
@@ -0,0 +1,4 @@
+from cryptography.fernet import Fernet
+
+key = b'uzeXlq2ZX6cJ8JSc06XbU_LpsHOP6TMqA5NpbMICBEI='
+fernet = Fernet(key)
---
+++ upload_routes.py
@@ -0,0 +1,29 @@
+
+from pathlib import Path
+from shutil import copyfileobj
+from fastapi import APIRouter, File, Form, UploadFile
+from fastapi.responses import HTMLResponse
+
+
+router = APIRouter()
+@router.get("/upload", response_class=HTMLResponse)
+async def upload_form():
+    return """
+    <form action="/upload" enctype="multipart/form-data" method="post">
+        <input name="file" type="file" multiple>
+        <input name="file_name" type="text">
+        <input type="submit">
+    </form>
+    """
+
+@router.post("/upload")
+async def upload_file(file: UploadFile = File(...), file_name: str = Form(...)):
+    if not file:
+        return {"message": "No upload file sent"}
+    suf = Path(file.filename).suffix
+    if not file_name:
+        file_name = file.filename
+    file_path = f"assets/{file_name+suf}"
+    with open(file_path, "wb") as buffer:
+        copyfileobj(file.file, buffer)
+    return {"filename": file_name+suf}
